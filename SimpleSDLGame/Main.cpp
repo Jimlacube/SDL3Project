924d08bee3b08a7f9e6c1ecc5cbb409eaf446345
Owen <Jimlacube@users.noreply.github.com>
2025-07-02 13:54:41 +0100

    new project setup commit

---
+++ SimpleSDLGame/Main.cpp
@@ -0,0 +1,67 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+int main(int argc, char* argv[])
+{
+    SDL_Window* window;                     // Declare a pointer
+    static SDL_Renderer* renderer = NULL;
+
+    bool done = false;
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL3 window",                  // window title
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    renderer = SDL_CreateRenderer(window, NULL);
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    while (!done) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+            }
+        }
+
+        // Do game logic, present a frame, etc.
+
+
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, SDL_ALPHA_OPAQUE_FLOAT);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE_FLOAT);
+
+        SDL_FRect rect;
+
+        rect.x = 10;
+        rect.y = 400;
+        rect.w = 100;
+        rect.h = 100;
+
+        SDL_RenderRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+    //Close and destroy renderer
+    SDL_DestroyRenderer(renderer);
+
+    // Clean up
+    SDL_Quit();
+    return 0;
+}
