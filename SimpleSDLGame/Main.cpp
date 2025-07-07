6b73e2b6e85b33b70ce06b5d5656172c4e6866be
Owen Rees <owenbenrees@gmail.com>
2025-07-07 14:55:37 +0100

    adding keyboard movement to the square

--- SimpleSDLGame/Main.cpp
+++ SimpleSDLGame/Main.cpp
@@ -1,5 +1,34 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <chrono>
+
+typedef std::chrono::high_resolution_clock Clock;
+
+static int keyDownUpdate(SDL_Event event, float& X, float& Y)
+{
+    switch (event.key.key)
+    {
+    case SDLK_UP:
+        Y = -1;
+        break;
+    case SDLK_DOWN:
+        Y = 1;
+        break;
+    case SDLK_LEFT:
+        X = -1;
+        break;
+    case SDLK_RIGHT:
+        X = 1;
+        break;
+
+    default:
+        X = 0;
+        Y = 0;
+        break;
+    }
+
+    return 0;
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,31 +56,79 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    
+
+    float InputX = 0;
+    float InputY = 0;
+
+    float rectX = 0;
+    float rectY = 0;
+
+    float speed = 100;
+
+    //Calculate delta
+    Clock::time_point prevTime;
+    float delta;
+
     while (!done) {
+
+        auto currentTime = Clock::now();
+        delta = std::chrono::duration_cast<std::chrono::nanoseconds>(currentTime - prevTime).count();
+        delta = delta / 1e+9;
+        prevTime = currentTime;
+
         SDL_Event event;
 
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 done = true;
             }
+
+            float X = 0;
+            float Y = 0;
+
+            switch (event.type)
+            {
+            case SDL_EVENT_KEY_DOWN:
+                //printf("Key press detected\n");
+
+                keyDownUpdate(event, X, Y);
+                
+                InputX += X * delta;
+                InputY += Y * delta;
+                
+                break;
+
+            case SDL_EVENT_KEY_UP:
+                //printf("Key release detected\n");
+                break;
+
+            default:
+                break;
+            }
+
         }
 
         // Do game logic, present a frame, etc.
+
+        SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 0, SDL_ALPHA_OPAQUE_FLOAT);
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE_FLOAT);
 
-        SDL_FRect rect;
+        SDL_FRect rect{};
 
-        rect.x = 10;
-        rect.y = 400;
+        rect.x = 10 + InputX * speed;
+        rect.y = 400 + InputY * speed;
         rect.w = 100;
         rect.h = 100;
 
         SDL_RenderRect(renderer, &rect);
 
         SDL_RenderPresent(renderer);
+
+        //printf("%f\n", rect.y);
     }
 
     // Close and destroy the window
@@ -63,3 +140,4 @@ int main(int argc, char* argv[])
     SDL_Quit();
     return 0;
 }
+
