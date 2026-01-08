a603ae7ee39dcebe0d8090dac7a0dbf10f9f9a51
Owen Rees <owenbenrees@gmail.com>
2026-01-08 22:23:46 +0000

    object h wip

--- SimpleSDLGame/Object.h
+++ SimpleSDLGame/Object.h
@@ -9,6 +9,10 @@ class Object : public Entity
 public:
 	Object();
 	Object(Vector2 pos, Vector2 dir);
+
+	void CollisionUpdate(float delta, Rect collisionRect, Vector2 newPosition);
+	//TODO move to object class
+	bool checkCollision(const Object* objectA, const Object* objectB);
 	
 	Vector2 position;
 	Vector2 direction;
